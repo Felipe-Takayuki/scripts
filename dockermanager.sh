c05863679177bf676e5483bf8c3b936756a2cf2d
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-07-09 14:47:36 -0300

    script de inicialização do docker

---
+++ dockermanager.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Verifica o status do serviço docker
+STATUS=$(systemctl is-active docker.service)
+
+if [ "$STATUS" = "active" ]; then
+  echo "Docker está ATIVO. Parando e desabilitando..."
+  sudo systemctl stop docker.socket docker.service
+  sudo systemctl disable docker.socket docker.service
+  echo "Docker parado e desabilitado."
+else
+  echo "Docker está INATIVO. Iniciando e habilitando..."
+  sudo systemctl enable docker.socket docker.service
+  sudo systemctl start docker.socket docker.service
+  echo "Docker iniciado e habilitado"
+  echo "Iniciando containers docker..."
+  sudo docker start db-mysql
+  echo "Containers inicializados"
+fi
